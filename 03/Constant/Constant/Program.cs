494c07d5b90727d0dd1c57beb41b3dca9b0aed9e
LeeMyungwoon <mu7625@naver.com>
2021-08-24 21:28:24 +0900

    Add Constant [Constant]

---
+++ 03/Constant/Constant/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Constant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string appleCountDescription = "바구니 안에 담긴 사과의 개수: ";
+            Console.Write(appleCountDescription);
+            int appleCount = 12;
+            Console.WriteLine(appleCount);
+
+            Console.WriteLine(-2);
+
+            string bacsketWeightDescription = "사과 바구니의 무게: ";
+            Console.WriteLine(bacsketWeightDescription);
+            double basketWeight = 1.32;
+            Console.WriteLine(basketWeight);
+        }
+    }
+}
